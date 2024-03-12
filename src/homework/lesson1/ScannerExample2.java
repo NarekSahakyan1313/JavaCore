e700ac957af7e85959bac9e98eb9233f9b8bdfd7
NarekSahakyan1313 <sahakyannarek777@gmail.com>
2024-03-12 11:12:16 -0700

    First project

---
+++ src/homework/lesson1/ScannerExample2.java
@@ -0,0 +1,15 @@
+package homework.lesson1;
+
+import java.util.Scanner;
+
+public class ScannerExample2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Greq dzer tan hastcen araqum@ hastatelu hamar");
+        String hastce= scanner.nextLine();
+        System.out.println("Ete duq hamadzayn eq dzer araqumi het apa greq ayo isk ete sxal eq greq noric");
+        String patasxan= scanner.nextLine();
+        System.out.println("Shnorhakalutyun vor ogtvel eq mer Online shop-ic");
+
+    }
+}
